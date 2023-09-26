2b285ead7c6b411c0f3498ab911054e9aca18f53
Serhieie <89160714+Serhieie@users.noreply.github.com>
2023-09-26 10:13:06 +0300

    last?

--- js/task-10.js
+++ js/task-10.js
@@ -22,49 +22,48 @@ function getRandomHexColor() {
 }
 
 const refs = {
-  input: document.querySelector('#controls input[type="number"][min="1"][max="100"]'),
+  input: document.querySelector("#controls input"),
   btnCreate: document.querySelector("button[data-create]"),
   btnDestroy: document.querySelector("button[data-destroy]"),
   objectInMultiply: document.querySelector("#boxes"),
-}
+};
 
-refs.objectInMultiply.style.display = 'flex';
-refs.objectInMultiply.style.flexWrap = 'wrap';
-refs.objectInMultiply.style.gap = '5px'
+refs.objectInMultiply.style.display = "flex";
+refs.objectInMultiply.style.flexWrap = "wrap";
+refs.objectInMultiply.style.gap = "5px";
 
 let boxSize = 30;
 const step = 10;
 // const boxes = [];
 
-refs.input.addEventListener('input', () => {
+refs.input.addEventListener("input", () => {
   const value = parseInt(refs.input.value, 10);
   if (isNaN(value) || value < 1) {
     refs.input.value = 1;
   } else if (value > 100) {
-    refs.input.value = 100; 
+    refs.input.value = 100;
   }
 });
 
 const createObjects = () => {
-  console.log()
-  for (let i = 1; i <= refs.input.value; i+=1) {
-    const box = document.createElement('div');
+  console.log();
+  for (let i = 1; i <= refs.input.value; i += 1) {
+    const box = document.createElement("div");
     box.style.marginTop = `20px`;
     box.style.backgroundColor = getRandomHexColor();
     box.style.width = `${boxSize}px`;
     box.style.height = `${boxSize}px`;
     // boxes.push(box)
-    console.log(box)
+    console.log(box);
     boxSize += step;
-    refs.objectInMultiply.append(box)
+    refs.objectInMultiply.append(box);
   }
-}
+};
 
 const destroyer = () => {
-  refs.objectInMultiply.innerHTML= ""
+  refs.objectInMultiply.innerHTML = "";
   window.location.reload();
-}
+};
 
-refs.btnCreate.addEventListener("click" , createObjects)
-refs.btnDestroy.addEventListener("click" , destroyer)
-
+refs.btnCreate.addEventListener("click", createObjects);
+refs.btnDestroy.addEventListener("click", destroyer);
